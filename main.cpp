8c5d1f9331738ff6cf9666d0a34c098978707f63
broadermat70 <54518170+broadermat70@users.noreply.github.com>
2020-01-15 15:59:11 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -21,3 +21,19 @@ int main()
 	return 0;
 
 }
+
+#include <iostream>
+
+using namespace std;
+
+int hello1Function(string name1)
+
+{
+
+	cout << "hello World " << name1 << endl;
+
+	return 0;
+
+}
+
+
